18061266ff2a085a4d451cedcc99590887137b84
Joaqim Planstedt <github@joaqim.xyz>
2021-09-14 11:36:24 +0200

    Minor additions to build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 BUILD_TYPE=${1:-Debug}
+if [ "${BUILD_TYPE}" == "run" ]; then
+  BUILD_TYPE="Debug"
+elif [ "${BUILD_TYPE}" == "Run" ]; then
+  BUILD_TYPE="Release"
+fi
 [ -d build ] || mkdir build
 set -e
 cd build && cmake .. -G Ninja -DCMAKE_BUILD_TYPE=$BUILD_TYPE && ninja
-[ "$1" == "run" ] && cd $BUILD_TYPE && ./bin/ideal --magnum-dpi-scaling 1.0 && exit 0
+[ "${1^^}" == "RUN" ] && cd $BUILD_TYPE && ./bin/ideal --magnum-dpi-scaling 1.0 && exit 0
 exit $?
